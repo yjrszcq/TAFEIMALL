daf4290d1020ffbd7d7fcd0fb7dd528b0539a88f
yjrszcq <yjr217@qq.com>
2025-05-16 21:50:57 +0800

    Redirect function

---
+++ src/main/java/cn/edu/xidian/tafei_mall/controller/RedirectController.java
@@ -0,0 +1,20 @@
+package cn.edu.xidian.tafei_mall.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("")
+public class RedirectController {
+    @Value("${web.web-url}")
+    String webUrl;
+
+    @GetMapping("")
+    public void returnToHomePage(HttpServletResponse httpResponse) throws IOException {
+        httpResponse.setStatus(HttpServletResponse.SC_OK);
+        httpResponse.sendRedirect(webUrl);
+    }
+}
